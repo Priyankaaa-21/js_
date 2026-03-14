f291d04eb06a310b9fbc7945891f9db5901a91c3
PRIYANKA <priyanka.chourasia.2105@gmail.com>
2026-03-14 17:09:10 +0000

    datatypes.md

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,11 @@
+//primitive
+//7-types ..String,Number,Boolean,null,undefined,Symbol,BigInt
+//reference//non-primitive
+//Arrys,Object,Functions
+///=======
+
+//Stack(primitive),Heap(non);
+let myYtname="hitesh"
+let anothername=myYtname;
+console.log(anothername);
+
