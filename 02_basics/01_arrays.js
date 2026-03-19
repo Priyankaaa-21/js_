7dee168cfaff9fd72dc5b6cf1f2356a6c0c0ec99
PRIYANKA <priyanka.chourasia.2105@gmail.com>
2026-03-19 05:03:27 +0000

    arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,17 @@
+//array
+const myArr=[0,1,2,3,4,5]
+//console.log(myArr[0]);
+//array method
+//myArr.push(6);
+//console.log(myArr);
+//pop,push,unshift(start pe value shift like push )
+//console.log("A",myArr);
+//const myn1=myArr.slice(1,3)
+//console.log(myn1);
+const myn1 = myArr.slice(1, 3)
+
+console.log(myn1);
+console.log("B ", myArr);
+const myn2 = myArr.splice(1, 3)
+console.log("C ", myArr);
+console.log(myn2);
