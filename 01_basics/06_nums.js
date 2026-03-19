f1d024e0381a9ef9f112f344e0c462c51be6ea90
PRIYANKA <priyanka.chourasia.2105@gmail.com>
2026-03-19 04:30:55 +0000

    base.js

---
+++ 01_basics/06_nums.js
@@ -0,0 +1,8 @@
+const score=400
+console.log(score);
+const balance=new Number(100);
+console.log(balance);
+console.log(balance.toString().length);
+const min =10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
